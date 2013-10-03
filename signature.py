7316febf521f586fc09b1bfffcef2b8f597db1ba
Eivind Fonn <evfonn@gmail.com>
2013-10-03 13:50:47 +0200

    Some more.

--- signature.py
+++ signature.py
@@ -3,11 +3,11 @@
 
 import random
 
-e = open("signature.txt", "w")
+e = open("/home/efonn/repos/signature/signature.txt", "w")
 
 e.write("Eivind Fonn - http://www.aligulac.com/ - HG J 59\nSeminar für Angewandte Mathematik\nRämistrasse 101, CH-8092 Zürich\n\n")
 
-f = open("signatures.txt")
+f = open("/home/efonn/repos/signature/signatures.txt")
 q = f.readlines()
 e.write(q[random.randint(0, len(q)-1)])
 
