54fb026753ac576698bdab6fc2a42a110909c054
Eivind Fonn <evfonn@gmail.com>
2014-05-05 14:53:42 +0200

    Portability

--- signature.py
+++ signature.py
@@ -2,12 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import random
+import os
 
-e = open("/home/efonn/repos/signature/signature.txt", "w")
+homedir = os.getenv('HOME')
 
-e.write("Eivind Fonn - http://www.aligulac.com/ - HG J 59<br>Seminar für Angewandte Mathematik<br>Rämistrasse 101, CH-8092 Zürich<br><br>")
+e = open(homedir + "/repos/signature/signature.txt", "w")
 
-f = open("/home/efonn/repos/signature/signatures.txt")
+e.write("Eivind Fonn - http://www.aligulac.com/<br>SINTEF Anvendt Matematikk<br>Strindvegen 4, 7034 Trondheim<br><br>")
+
+f = open(homedir + "/repos/signature/signatures.txt")
 q = f.readlines()
 e.write(q[random.randint(0, len(q)-1)])
 
